34c853807ca27a3ee8fc46d86745f2893a3cea98
Wiaczeslaw <105964596+vkubarevc@users.noreply.github.com>
2022-05-21 00:22:37 -0700

    Add files via upload

---
+++ ff.cpp
@@ -0,0 +1,22 @@
+/*
+Purpose:Calculates the distance traveled under freefall
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+	double time, distance; 
+	cout << "Enter the time in seconds.\n";
+	cin >> time;
+	//Acceleration=32
+	distance = (32*time*time)/2;
+	cout << "Distance traveled under freefall is "<< distance <<" feet per second squared." << endl;
+
+return 0;
+
+}
+
+
